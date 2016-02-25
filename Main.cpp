f1ce89da7919a14d2e7fb439dbf2885dacd600ee
jonathancho <levels4you@gmail.com>
2016-02-24 17:49:13 -0800

    Spinning triangle drawing on screen!

--- Main.cpp
+++ Main.cpp
@@ -16,7 +16,12 @@ float  g_dLastTime = 0.0f;
 
 HWND Hwnd = NULL;
 
+// Function Declarations
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+void InitOpenGL(HWND hWnd, HDC* hDC, HGLRC* hRC);
+void ShutDownOpenGL(HWND hWnd, HDC hDC, HGLRC hRC);
+void UpdateScene(float dt);
+void Render(float& theta, HDC hDC);
 
 int WINAPI WinMain(HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
@@ -27,6 +32,9 @@ int WINAPI WinMain(HINSTANCE hInstance,
 
 	WNDCLASSEX winClass;
 	MSG        uMsg;
+	HDC		   hDC;
+	HGLRC	   hRC;
+	float theta = 0.0f;
 
 	memset(&uMsg, 0, sizeof(uMsg));
 
@@ -56,7 +64,7 @@ int WINAPI WinMain(HINSTANCE hInstance,
 	ShowWindow(Hwnd, nCmdShow);
 	UpdateWindow(Hwnd);
 
-	//Init();
+	InitOpenGL(Hwnd, &hDC, &hRC);
 
 	while (uMsg.message != WM_QUIT)
 	{
@@ -67,15 +75,15 @@ int WINAPI WinMain(HINSTANCE hInstance,
 		}
 		else
 		{
-			g_dCurrentTime = (double)timeGetTime();
+			g_dCurrentTime = (float)timeGetTime();
 			g_fElapsedTime = (float)((g_dCurrentTime - g_dLastTime) * 0.001);
 			g_dLastTime = g_dCurrentTime;
-			//UpdateScene(g_fElapsedTime);
-			//Render();
+			UpdateScene(g_fElapsedTime);
+			Render(theta, hDC);
 		}
 	}
 
-	//ShutDown();
+	ShutDownOpenGL(Hwnd, hDC, hRC);
 
 
 	UnregisterClass(L"ModelViewerClass", winClass.hInstance);
@@ -104,4 +112,63 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 	}
 	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+void InitOpenGL(HWND hWnd, HDC* hDC, HGLRC* hRC)
+{
+	PIXELFORMATDESCRIPTOR pfd;
+	int format;
+
+	// get the device context (DC)
+	*hDC = GetDC(hWnd);
+
+	// set the pixel format for the DC
+	ZeroMemory(&pfd, sizeof(pfd));
+	pfd.nSize = sizeof(pfd);
+	pfd.nVersion = 1;
+	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.cColorBits = 24;
+	pfd.cDepthBits = 16;
+	pfd.iLayerType = PFD_MAIN_PLANE;
+	format = ChoosePixelFormat(*hDC, &pfd);
+	SetPixelFormat(*hDC, format, &pfd);
+
+	// create and enable the render context (RC)
+	*hRC = wglCreateContext(*hDC);
+	wglMakeCurrent(*hDC, *hRC);
+}
+
+void ShutDownOpenGL(HWND hWnd, HDC hDC, HGLRC hRC)
+{
+	wglMakeCurrent(NULL, NULL);
+	wglDeleteContext(hRC);
+	ReleaseDC(hWnd, hDC);
+}
+
+void UpdateScene(float dt)
+{
+	// Update scene based on user input
+	return;
+}
+
+void Render(float& theta, HDC hDC)
+{
+	// OpenGL animation code goes here
+
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glPushMatrix();
+	glRotatef(theta, 0.0f, 0.0f, 1.0f);
+	glBegin(GL_TRIANGLES);
+	glColor3f(1.0f, 0.0f, 0.0f); glVertex2f(0.0f, 1.0f);
+	glColor3f(0.0f, 1.0f, 0.0f); glVertex2f(0.87f, -0.5f);
+	glColor3f(0.0f, 0.0f, 1.0f); glVertex2f(-0.87f, -0.5f);
+	glEnd();
+	glPopMatrix();
+
+	SwapBuffers(hDC);
+
+	theta += 1.0f;
 }
